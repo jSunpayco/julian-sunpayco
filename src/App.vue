47c296b6d889aec804769b816929b784fe110aed
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-12-03 13:27:18 +0800

    Responsive nav bar (burger)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script>
 import {RouterView} from 'vue-router'
+
+export default {
+	data() {
+		return {
+			open: false
+		};
+	},
+	methods: {
+		toggle() {
+			this.open = !this.open;
+		}
+	}
+};
 </script>
 
 <template>
-  <!-- https://tailwindsites.com/templates/outofthebox -->
-  <!-- https://supacode.dev/#contact -->
-  <!-- https://nikitar.tech/#edu-exp -->
-  <!-- https://stackoverflow.com/questions/70824120/scroll-to-section-from-child-component-in-vue-js -->
-  <!-- https://vuejs.org/guide/essentials/reactivity-fundamentals.html#declaring-reactive-state -->
-  <!-- https://medium.com/ascentic-technology/sticky-scrollspy-table-of-contents-in-vue-js-73d2d6f0a1cd -->
   
   <div class="w-screen fixed z-10">
     <div id="naviBar" class="container px-4 mx-auto max-w-7xl">
@@ -22,9 +29,40 @@ import {RouterView} from 'vue-router'
                 <li><a class="naviLink" href="#works">Projects</a></li>
                 <li><a class="naviLink" href="#contact">Contact</a></li>
             </ul>
+            <div class="mx-5">
+              <button class="lg:hidden block text-black hover:text-customBeigeDarker" @click.prevent="toggle()">
+                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                </svg>
+              </button>
+            </div>
         </nav>
     </div>
 
+  </div>
+
+  <div class="sideNaviContainer" :class="[open ? 'max-w-full' : 'max-w-0']">
+    <div class="fixed inset-0 bg-gray-800 opacity-25" :class="[open ? 'visible' : 'invisible']"></div>
+    <nav class="sideNavi"
+      :class="[open ? 'visible' : 'invisible']">
+        <div class="flex items-center mb-12">
+            <button class="navbar-close" @click.prevent="toggle()">
+                <svg class="h-6 w-6 cursor-pointer hover:text-customBeigeDarker" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            </button>
+        </div>
+        <div>
+            <ul>
+                <li class="mb-1"><a @click="toggle()" class="sideNaviText" href="#landing">Home</a></li>
+                <li class="mb-1"><a @click="toggle()" class="sideNaviText" href="#about">About</a></li>
+                <li class="mb-1"><a @click="toggle()" class="sideNaviText" href="#works">Projects</a></li>
+                <li class="mb-1"><a @click="toggle()" class="sideNaviText" href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
   </div>
 
   <transition name="fade" mode="out-in">
