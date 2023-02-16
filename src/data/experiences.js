fc35f3afd969b936c59c8a713c446f3a684e176e
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-02-15 20:28:54 -0800

    Update slice description

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -66,7 +66,7 @@ const experiences = [
         linkName: "demo",
         link: "https://slice-recipes.vercel.app/",
         year: "Jan 2023",
-        description: "My next take on creating a project was to further improve my understanding on React, JavaScript, and web development in general. This was along with the incentive to create something that has the potential to be useful me sometime in the future. Slice, is a recipe sharing application that aims to be of use those who want to safekeep and share recipes that they have created, or was taught to them for future reference. However, you'd need not to worry if you have nothing to share as you, the public, will still have access to the different recipes shared by the application's users. Small note; As of Feb 4, 2023, deleting recipes, and accounts are not available.",
+        description: "My next take on creating a project was to further improve my understanding on React, JavaScript, and web development in general. This was along with the incentive to create something that has the potential to be useful me sometime in the future. Slice, is a recipe sharing application that aims to be of use those who want to safekeep and share recipes that they have created, or was taught to them for future reference. However, you'd need not to worry if you have nothing to share as you, the public, will still have access to the different recipes shared by the application's users. Note; Currently, the comments feature is under works.",
         techs: ["React", "TypeScript", "Firebase"],
         preview: 'slice.png',
         type: 'Personal Project'
