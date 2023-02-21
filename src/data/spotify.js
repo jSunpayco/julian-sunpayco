5db71bc4bbcbbfe6cb692b8de5f4ff5c851aab9a
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-02-20 20:05:50 -0800

    Converted images from webp to png
    Images with transparent backgrounds were not converted. Includes heritage image in the landing page, and the images for the socials.

--- src/data/spotify.js
+++ src/data/spotify.js
@@ -2,7 +2,7 @@ const spotify = [
     {
         id: 1,
         name: 'Heize',
-        img: 'heize.png',
+        img: 'heize.webp',
         link: 'https://open.spotify.com/track/1WT5I6vlWjYW2cnhR1UkVA?si=777c11f411c54b78',
         preview: 'shutUpAndGroove.mp3',
         isPlaying: false
@@ -10,7 +10,7 @@ const spotify = [
     {
         id: 2,
         name: 'Men I Trust',
-        img: 'menitrust.png',
+        img: 'menitrust.webp',
         link: 'https://open.spotify.com/track/3rRAR3YHRFShjEaHP0o9Xi?si=11bc9f4661904f22',
         preview: 'tailwhip.mp3',
         isPlaying: false
@@ -18,7 +18,7 @@ const spotify = [
     {
         id: 3,
         name: 'Sunset Rollercoaster',
-        img: 'sunset.png',
+        img: 'sunset.webp',
         link: 'https://open.spotify.com/track/2uSYrhAHtVDBdsnkLqdJOO?si=22809182426a44ea',
         preview: 'underTheSkin.mp3',
         isPlaying: false
@@ -26,7 +26,7 @@ const spotify = [
     {
         id: 4,
         name: 'Jinsang',
-        img: 'jinsang.png',
+        img: 'jinsang.webp',
         link: 'https://open.spotify.com/track/46IqTp7drU0fL0NXuNLiJN?si=3b9a1ae19ebb4a2b',
         preview: 'flowOn.mp3',
         isPlaying: false
@@ -34,7 +34,7 @@ const spotify = [
     {
         id: 5,
         name: 'Radiohead',
-        img: 'radiohead.png',
+        img: 'radiohead.webp',
         link: 'https://open.spotify.com/track/10nyNJ6zNy2YVYLrcwLccB?si=dcd650de41814681',
         preview: 'noSurprises.mp3',
         isPlaying: false
