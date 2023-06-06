3e4a772940c97945785bb21252f4d86ba54529f7
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-06-06 13:12:11 -0700

    Filter styling and animation

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -2,10 +2,14 @@
 import exps from "../data/experiences";
 import gsap from 'gsap'
 import Work from '@/components/Work.vue'
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
 
 export default{
   data() {
 		return {
+      target: null,
+      dropwdown: null,
       expFiltered: exps,
       isOpened: false,
       currFilter: 'All'
@@ -19,12 +23,31 @@ export default{
       .to("#singleWork", {opacity: 100, duration: 0.3})
     
     this.tl.restart();
+
+    this.target = this.$refs.target;
+    this.dropwdown = this.$refs.dropwdown;
+    document.addEventListener('click', this.handleOutsideClick);
   },
   components: { Work },
 	methods: {
 		toggle() {
 			this.isOpened = !this.isOpened;
+      if (this.isOpened) {
+        gsap.set(this.dropwdown, { height: 'auto' });
+        gsap.from(this.dropwdown, { height: 0, duration: 0.3 });
+      } else {
+        gsap.to(this.dropwdown, { height: 0, duration: 0.3 });
+      }
 		},
+    toggleClose(){
+      this.isOpened = false;
+      gsap.to(this.dropwdown, { height: 0, duration: 0.3 });
+    },
+    handleOutsideClick(event) {
+      if (this.target && !this.target.contains(event.target)) {
+        this.toggleClose();
+      }
+    },
     filterWorks(type) {
 
       if(type == 'All')
@@ -36,7 +59,10 @@ export default{
       this.toggle()  
       return
     }
-	}
+	},
+  beforeUnmount() {
+    document.removeEventListener('click', this.handleOutsideClick);
+  }
 }
 </script>
 
@@ -45,14 +71,13 @@ export default{
     <h1 id="titleName" class="worksTitle">My Experiences</h1>
 
     <div class="flex justify-center lg:justify-end mt-8">
-      <button class="flex flex-row justify-between border-2 border-customRose rounded-lg p-2 w-48 bg-customJet" @click.prevent="toggle()" aria-label="FilterWorks">
-        <p class="select-none text-white">Filter: {{currFilter}}</p>
+      <button class="flex flex-row justify-between rounded-lg p-2 w-48 bg-customJet" @click.prevent="toggle()" aria-label="FilterWorks" ref="target">
+        <p class="select-none text-white ml-1">Filter: {{currFilter}}</p>
         <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg></div>
       </button>
     </div>
-
     
-    <div class="absolute -lg:centerAbsItem lg:right-10 mx-auto w-48 mt-5 rounded-lg bg-customJet z-20" :class="[isOpened ? 'visible' : 'invisible']">
+    <div class="absolute -lg:centerAbsItem lg:right-10 mx-auto w-48 mt-2 rounded-lg bg-customJet z-20 overflow-hidden h-0" ref="dropwdown">
       <ul>
         <li class="p-3 hover:bg-customGreenDark rounded-t-lg cursor-pointer" @click.prevent="filterWorks('All')" :class="[currFilter == 'All' ? 'bg-customGreenDark' : 'bg-transparent']">
           <p class="select-none text-white">All</p>
